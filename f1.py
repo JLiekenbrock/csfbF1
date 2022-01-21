3764fbd9e6f1e7bf0341f1e9880bd12b4ce592b9
Jan Liekenbrock <jan.liekenbrock@protonmail.com>
2022-01-21 16:24:28 +0100

    add DAG

--- f1.py
+++ f1.py
@@ -29,20 +29,45 @@ print("""
 """
 )
 #5
+
+from re import X
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id, col, lag, when
 
-if __name__ == "__main__":
-    """
-        Usage: pi [partitions]
-    """
-    spark = SparkSession\
-        .builder\
-        .appName("NLP_Pipeline")\
-        .getOrCreate()
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
 
-    threads = 2
+sc = spark.sparkContext
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+print("#4 a\n")
 
-    spark.stop()
+df1 = spark.read.text("Shakespeare.txt") \
+    .withColumn("rowId", monotonically_increasing_id())  \
+    .filter(col("rowId") >245 )
+
+df1.show()
+
+print("#4 b\n")
+
+copyright = '''<<THIS ELECTRONIC VERSION OF THE COMPLETE WORKS OF WILLIAM
+SHAKESPEARE IS COPYRIGHT 1990-1993 BY WORLD LIBRARY, INC., AND IS
+PROVIDED BY PROJECT GUTENBERG ETEXT OF ILLINOIS BENEDICTINE COLLEGE
+WITH PERMISSION.  ELECTRONIC AND MACHINE READABLE COPIES MAY BE
+DISTRIBUTED SO LONG AS SUCH COPIES (1) ARE FOR YOUR OR OTHERS
+PERSONAL USE ONLY, AND (2) ARE NOT DISTRIBUTED OR USED
+COMMERCIALLY.  PROHIBITED COMMERCIAL DISTRIBUTION INCLUDES BY ANY
+SERVICE THAT CHARGES FOR DOWNLOAD TIME OR FOR MEMBERSHIP.>>'''
+
+copyright = [line for line in copyright.split("\n")]
+
+df1.filter(col("value").isin(copyright)).show()
+
+df1=df1.filter(~col("value").isin(copyright)).show()
+
+print("#4 c\n")
+
+df1.withColumn("play", col("value").contains("by William Shakespeare") ) 
+
