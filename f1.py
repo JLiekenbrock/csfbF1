dbf6d3979e71d33b8d06b0219d208ec29d95b300
Jan Liekenbrock <jan.liekenbrock@protonmail.com>
2022-01-22 13:47:09 +0100

    add index

--- f1.py
+++ f1.py
@@ -21,7 +21,7 @@ from re import X
 from turtle import xcor
 from pandas import isnull
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import monotonically_increasing_id, col, lag, when, desc,spark_partition_id,split, explode
+from pyspark.sql.functions import monotonically_increasing_id, col, lag, when, desc,spark_partition_id,split, explode,countDistinct
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number
 
@@ -84,19 +84,19 @@ df1.filter(col("title")=="").show()
 df1.groupBy("title").count().show()
 
 df1.rdd.getNumPartitions()
-df1=df1.drop("Lag","play","rowId").dropna()
+df1=df1.drop("Lag","play").dropna()
 
 df1.rdd.getNumPartitions()
 
-partitions = df1.agg(countDisentencestinct(col("title"))).collect()[0][0]
+partitions = df1.agg(countDistinct(col("title"))).collect()[0][0]
 
 df1=df1.repartition(partitions,"title")
 
 df1.rdd.getNumPartitions()
 
-df1=df1.select(df1.title,df1.value,split(df1.value, '\s+').alias('split'))
+df1=df1.select(df1.rowId,df1.title,df1.value,split(df1.value, '\s+').alias('split'))
 
-df1=df1.select(df1.title,df1.value,explode(df1.split).alias('word'))
+df1=df1.select(df1.rowId,df1.title,df1.value,explode(df1.split).alias('word'))
 df1.show()
 df1=df1.where(df1.word != '')
 df1.show()
